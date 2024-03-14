e139382ba29a2f19a1493d01e061d698522d228f
Luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-14 20:50:11 -0300

    segundo exemplo

--- metodo_1.js
+++ metodo_1.js
@@ -1,4 +1,15 @@
+// nesse metodo estamos mesclando duas Arrays
 let carro = ['Ford', 'Nissan', 'Honda', 'Chevrolet']
 let carro2 = ['Fiat', 'Peugeot', 'Ford', 'Renault']
+// aqui estou falando que marcas = mesclagem de carro + carro2
 let marcas = carro.concat(carro2)
+// aqui eu estou exibindo as mascas mescladas 
 console.log(marcas)
+
+
+
+let numero = ['1', '7', '10', '13', '23']
+let numero2 = ['11', '17', '20', '33', '43']
+let numeros = numero.concat(numero2)
+console.log(numeros)
+
