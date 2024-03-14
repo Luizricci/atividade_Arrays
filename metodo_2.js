27682d1bebc0f3374a4ba86bad2c4331ae0a527c
Luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-14 20:55:43 -0300

    segundo exemplo

--- metodo_2.js
+++ metodo_2.js
@@ -2,6 +2,11 @@
 // ou seja o primeiro numero que seja maior que 3 ele exibirá
 let numeros = ['1', '7', '10', '13', '23']
 // aqui estou dizendo que o find irá procurar por elemento que seja > 3 
-let encontrar = numeros.find((elemento) => elemento > 3 )
+let encontrar = numeros.find((elemento) => elemento > 3)
 // aqui eu exibo o numero que o find me mostrar
 console.log(encontrar)
+
+
+let carro = ['Ford', 'Nissan', 'Honda', 'Chevrolet']
+let encontrar2 = carro.find((elemento) => elemento =='Honda')
+console.log(encontrar2)
