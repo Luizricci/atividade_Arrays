afd693d5d441b1793a17ed9d099aa91bdc295bc9
Luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-14 21:09:09 -0300

    metodo de inverter a ordem de itens

---
+++ metodo_4.js
@@ -0,0 +1,10 @@
+// nesse metodo utilizei o toReversed para inverter a ordem de traz para frente 
+let carro = ['Ford', 'Nissan', 'Honda', 'Chevrolet']
+// aqui estou dizendo que a função inverteritem será a Array carro invertida 
+let inverterItem = carro.toReversed();
+// aqui estou exibindo a função
+console.log(inverterItem)
+
+let numeros = ['1', '2', '3']
+let inverterNumero = numeros.toReversed()
+console.log(inverterNumero)
