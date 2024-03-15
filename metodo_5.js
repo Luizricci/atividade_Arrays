a16264c73b6e9c724b878f4547f286d16f060b7a
Luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-15 14:48:45 -0300

    metodo de organização da Array

---
+++ metodo_5.js
@@ -0,0 +1,16 @@
+// o metodo de sort organiza a Array de forma numerica, alfabetica, etc.
+let alimentos = ['Caju', 'Amora', 'Banana',]
+// aqui estou atribuindo que a Array alimetntos irá ser organizada
+alimentos.sort()
+// aqui eu exibo o resultado
+console.log(alimentos)
+
+// organização numerica 
+let numeros = ['3', '4', '1', '2']
+numeros.sort()
+console.log(numeros)
+// o metodo sort tem como prioridade numeros 
+
+let alimentoNumero = ['1 Maça', 'Banana', '2 Abacates', 'Lixia']
+alimentoNumero.sort()
+console.log(alimentoNumero)
