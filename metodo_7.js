5056810f5607435aa042a91e7d689e2263dd7ef1
Luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-15 20:53:13 -0300

    metodo de remoção do primeiro objeto ou evidenciação do mesmo

---
+++ metodo_7.js
@@ -0,0 +1,11 @@
+// esse metodo retira o primerio elemento de uma Array
+let comida = ['Caju', 'Amora', 'Banana',]
+// aqui estou dizendo que na Array comida irá acontescer o metodo shift
+let primeiro = comida.shift();
+console.log(comida);
+
+// mas eu consigo tambem exibir apenas o primeiro numero sem remover ele da Array como:
+
+let numero = [ '2', '3', '4', '6', '8']
+let primeiroItem = numero.shift();
+console.log(primeiroItem);
