a277413fc135c838f2e4f926ff21d23590dc9811
Luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-15 21:25:44 -0300

    metodo de verificação de itens na Array

---
+++ metodo_8.js
@@ -0,0 +1,23 @@
+
+let comida = ['Caju', 'Amora', 'Banana',]
+let tem = comida.includes('Amora')
+console.log(comida)
+console.log(tem)
+
+
+
+
+
+let a = 10
+let b = 1 
+// aqui eu estou dizendo que a Array começara vazia
+let Array = []
+// fiz um laço de repetção para contar de 1 - 10
+for(let i=b; i<=a; i++){
+    console.log(i)
+    // aqui eu estou dizendo que adicionará o valor de i na Array
+    Array.push(i)
+}
+// aqui eu estou fazendo a verificação se 3 está encluido na Array
+console.log(Array.includes(3))
+
