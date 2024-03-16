42ba60e29edf98c328c6ec6f4128374efb981df4
Luizricci <luizaureliano.tds1.senai@gmail.com>
2024-03-15 21:52:47 -0300

    esse metodo aplica um filtro na Array

---
+++ metodo_9.js
@@ -0,0 +1,8 @@
+let comida = ['Caju', 'Amora', 'Banana',]
+// neste metodo eu estou filtrando e exibindo todas as palavras que contem mais de 4 letras
+let filtro = comida.filter((comida) => comida.length > 4)
+console.log(filtro)
+
+let carro = ['Ford', 'Nissan', 'Honda', 'Chevrolet']
+let filtro2 = carro.filter((carro) => carro.length > 4)
+console.log(filtro2)
